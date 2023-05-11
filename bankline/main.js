0e67e1f7f10250ffc6b029b0ff9d2e3433ce7a1e
Nikko <nikoaguiar900@gmail.com>
2023-05-11 17:23:21 -0300

    Update

--- bankline/main.js
+++ bankline/main.js
@@ -1,6 +1,9 @@
 const nomeBeneficiario = document.getElementById('nome_beneficiario');
 let formValido = false;
 const form = document.getElementById('form_dep');
+const containerMensagemSucesso = document.querySelector('.mensagem_sucesso');
+const containerMensagemErro = document.querySelector('.mensagem_erro');
+const numeroConta = document.getElementById('numero_conta');
 
 function validaNome(nomeCompleto){
     const nomeComoArray = nomeCompleto.split(' ');
@@ -12,22 +15,26 @@ form.addEventListener('submit', function(e){
     
     e.preventDefault();
     
-    const numeroConta = document.getElementById('numero_conta');
+    
     const valorDeposito = document.getElementById('valor_deposito');
     const mensagemSucesso = `Valor de: <b>${valorDeposito.value}</b> foi depositado para o cliente <b>${nomeBeneficiario.value}</b> - conta nº: <b>${numeroConta.value}</b>`;
+    const mensagemErro = `O nome não está completo!`
     formValido = validaNome(nomeBeneficiario.value);
 
     if(formValido){
-        const containerMensagemSucesso = document.querySelector('.mensagem_sucesso');
         containerMensagemSucesso.innerHTML = mensagemSucesso;
         containerMensagemSucesso.style.display = 'block';
+        containerMensagemErro.style.display = 'none';
 
         nomeBeneficiario.value = '';
         numeroConta.value = '';
         valorDeposito.value = '';
     }else{
-        nomeBeneficiario.style.border = '1px solid red';
-        document.querySelector('.mensagem_erro').style.display = 'block';
+        containerMensagemErro.innerHTML = mensagemErro;
+        containerMensagemErro.style.display = 'block';
+        containerMensagemSucesso.style.display = 'none';
+        // nomeBeneficiario.style.border = '1px solid red';
+        // document.querySelector('.mensagem_erro').style.display = 'block';
     }
 })
 
@@ -37,9 +44,26 @@ nomeBeneficiario.addEventListener('keyup', function(e){
 
     if(!formValido){
         nomeBeneficiario.classList.add('error');
-        document.querySelector('.mensagem_erro').styple.display = 'block';
+        document.querySelector('.mensagem_erro').style.display = 'block';
     }else{
         nomeBeneficiario.classList.remove('error');
-        document.querySelector('.mensagem_erro').styple.display = 'none';
+        document.querySelector('.mensagem_erro').style.display = 'none';
+    }
+})
+
+let containerNumeroConta = [];
+
+numeroConta.addEventListener('keyup', function(e){
+    console.log(e.target.value);
+
+    containerNumeroConta.push(numeroConta.value);
+    console.log(containerNumeroConta);
+
+    if(containerNumeroConta.length < 8){
+        numeroConta.classList.add('error');
+        document.querySelector('.mensagem_erro_numero').style.display = 'block';
+    }else{
+        numeroConta.classList.remove('error');
+        document.querySelector('.mensagem_erro_numero').style.display = 'none';
     }
 })
